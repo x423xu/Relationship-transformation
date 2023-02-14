6b2346e649dd46ab6ee7d127852eea439d6f0413
xiaoyu <15271887443@163.com>
2023-02-13 20:49:18 -0500

    update

---
+++ model/RelTrans.py
@@ -0,0 +1,103 @@
+import torch
+from typing import Dict
+import torch.nn as nn
+from lib.synsin.models.z_buffermodel import ZbufferModelPts
+# from ..lib.synsin.options.train_options import ArgumentParser
+from .networks.RelSpa import RSModel, Reprojector
+
+class Hook():
+    def __init__(self, d=None) -> None:
+        self._data = d
+
+    @property
+    def data(self):
+        return self._data
+    
+    @data.setter
+    def data(self, d):
+        self._data = d
+
+class NewZbufferModelPts(ZbufferModelPts):
+    def __init__(self, opt):
+        super().__init__(opt)
+        self.hook = Hook()
+        """
+        freeze unused parts
+        """
+        self._freeze_module(self.encoder)
+        self._freeze_module(self.projector)
+        self._freeze_module(self.pts_regressor)
+    
+    def _freeze_module(self, module):
+        for p in module.parameters():
+            p.requires_grad = False
+
+    def forward(self, batch, rel_embedding):
+        input_img = batch['images'][0]
+        K = batch['K']
+        K_inv = batch['Kinv']
+        input_RT = batch['P'][0]
+        input_RTinv = batch['Pinv'][0]
+        output_RT = batch['P'][1]
+        output_RTinv = batch['Pinv'][1]
+        
+        fs = rel_embedding
+        regressed_pts = (
+                    nn.Sigmoid()(self.pts_regressor(input_img))
+                    * (self.opt.max_z - self.opt.min_z)
+                    + self.opt.min_z
+                )
+        gen_fs = self.pts_transformer.forward_justpts(
+            fs,
+            regressed_pts,
+            K,
+            K_inv,
+            input_RT,
+            input_RTinv,
+            output_RT,
+            output_RTinv,
+            self.hook
+        )
+        pts3d = self.hook.data
+        return gen_fs, pts3d
+
+class RelTrans(nn.Module):
+    def __init__(
+        self,args
+    ):
+        super().__init__()
+        self.args = args
+        '''
+        Relationship Spatializer
+        '''
+        self.relationship_spatializer = RSModel()
+        self.rel_embedding = nn.Sequential(
+            nn.Conv2d(256,256,3,1,1),
+            nn.LeakyReLU(),
+            nn.Conv2d(256,256,3,1,1)
+        )
+
+        '''
+        z_buffer
+        '''
+        MODEL_PATH = 'lib/synsin/modelcheckpoints/realestate/zbufferpts.pth'
+        opts = torch.load(MODEL_PATH)['opts']
+        self.z_buffer = NewZbufferModelPts(opts)
+
+        '''
+        reprojector
+        '''
+        self.reprojector = Reprojector()
+
+        '''
+        bbox_regressor
+        '''
+
+    def forward(self, batch: Dict):
+        rel_features = batch['rel_features'][0]
+        bbox = batch['bbox'][0]
+        spatial_rel = self.relationship_spatializer(rel_features, bbox, [self.args.W, self.args.W])
+        rel_embedding = self.rel_embedding(spatial_rel)
+        z_buffer, pts3d = self.z_buffer(batch, rel_embedding)
+        fs_result = self.reprojector(z_buffer)
+        return fs_result, pts3d

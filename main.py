62645f95df22dd0117bf7b528cb7d6c1fc3492fb
xiaoyu <15271887443@163.com>
2023-02-16 16:23:04 -0500

    update

--- main.py
+++ main.py
@@ -14,12 +14,22 @@ from utils.pl_config import set_arguments_pl
 '''
 
 '''
-
+'''when in ddp training, set all tasks have same limited train and val batch size'''
+def set_limited_train_val(args):
+    d = {}
+    if args.accelerator == 'ddp':
+        if args.train_size ==8 and args.val_size==8:
+            d.update({'limit_val_batches':116})
+        if args.train_size ==4 and args.val_size==4:
+            d.update({'limit_val_batches':232})
+    return d
 def main():
     dataloader = make_data(args)
     model = make_model(args)
 
     pl_args = set_arguments_pl(args)
+    val_size = set_limited_train_val(args)
+    pl_args.update(**val_size)
     trainer = pl.Trainer(**pl_args)
     if args.mode == 'train':
         trainer.fit(model, dataloader)
